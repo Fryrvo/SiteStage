82734bffbfdc49d62dd978b0300a7d6964eb4ecd
Fryrvo <fryrvo@fryrvo.com>
2026-09-16 20:46:49 +0800

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 import icon from 'astro-icon';
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://fryrvo.com',
   output: 'server',
